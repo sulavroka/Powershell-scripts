850d8cb9d20830bbf565be424f3f9c8dd00b7c0e
Tekendra Roka <sulove_123@hotmail.com>
2019-04-23 15:28:03 +0100

    Update Office365.ps1

--- Office365.ps1
+++ Office365.ps1
@@ -1,3 +1,9 @@
 #Connect powershell to office 365
 Install-Module -Name AzureAD
 Connect-AzureAD
+
+
+
+#Run these commands to connect to Exchange Online.
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
