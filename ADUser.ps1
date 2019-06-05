aad9645bce2944ac25483b6967b00866181edfef
Tekendra Roka <sulove_123@hotmail.com>
2019-06-05 11:05:06 +0100

    Update ADUser.ps1

--- ADUser.ps1
+++ ADUser.ps1
@@ -1,3 +1,6 @@
 #Query user name with Apostrophe
 $readvalue = "William O'Brian"
 (Get-ADUser -Filter "Name -eq `$readvalue" -Properties *).SamaccountName
+
+#get ou of the user
+ get-aduser Tnikov| Select-Object -Property  DistinguishedName
