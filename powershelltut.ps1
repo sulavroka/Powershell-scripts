815a421211b4fd14d3ab422cdb76f5ab0c62d486
Tekendra Roka <sulove_123@hotmail.com>
2019-06-07 16:05:38 +0100

    Create powershelltut.ps1

---
+++ powershelltut.ps1
@@ -0,0 +1,35 @@
+#Variables to store your stuff
+#Assigning a variable
+$hello =2 
+${hello} = "Hello"
+
+#Output a Variable 
+$hello
+${hello}
+
+write-output $hello
+
+#Strongly type variable
+[String] $MyName = "sulav"
+[int] $Oops = "Opps"
+[string] $ComputerName = Read-host "Enter Computer Name"
+Write-Output $ComputerName
+
+#types
+# $hello = "02/03/1995"
+#[datetime]$d = $hello
+# $d.dayoftheweek
+
+#Resolving Variables
+$hello = "wow"
+
+"i cant believe this is $hello"
+'i cant believe this is $hello'
+
+#variable are like array
+#for example $hello = get-service
+#$hello[0]
+
+#here string basically subexpression like in haskell  2+2 "2 + 2" $("2+2") 
+#putting arugments on the paratmeters just like pipes
+Get-service -name bits -ComputerName (Get-Content c:\computers.txt)
