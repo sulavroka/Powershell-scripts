8684e7676c9b0cdd8e886863d2e074dbf9b5f375
Tekendra Roka <sulove_123@hotmail.com>
2019-06-10 09:38:23 +0100

    Update eventviewer.ps1

--- eventviewer.ps1
+++ eventviewer.ps1
@@ -1,2 +1,6 @@
 #get user event
 Get-EventLog -Log System | Where {$_.UserName -ne$null} |  Select-Object -Property Timegenerated, Username, EventID
+
+#user event with event id 
+#https://docs.microsoft.com/en-us/windows-server/identity/ad-ds/plan/appendix-l--events-to-monitor
+Get-EventLog -Log System | Where {$_.username -eq "antal\troka" -and $_.eventid -eq 7045 } |  Select-Object -Property Timegenerated, Username, EventID
