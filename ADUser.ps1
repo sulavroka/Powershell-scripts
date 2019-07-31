d5f88b8389b799b77cf3be5b4df20218e1f48618
Tekendra Roka <sulove_123@hotmail.com>
2019-07-31 09:22:14 +0100

    Update ADUser.ps1

--- ADUser.ps1
+++ ADUser.ps1
@@ -4,3 +4,7 @@ $readvalue = "William O'Brian"
 
 #get ou of the user
  get-aduser Tnikov| Select-Object -Property  DistinguishedName
+ 
+ #get aduser created gt than 
+ get-aduser -Filter * -properties whencreated | where{$_.whenCreated -ge (get-date "January 1, 2019")}| FT Name ,whencreated,distinguishedname
+
