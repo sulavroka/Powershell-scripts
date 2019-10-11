9714a0728d393c5b560fe0657c1e9c3a6ff5894a
Tekendra Roka <sulove_123@hotmail.com>
2019-10-11 10:59:07 +0100

    Update exchange.ps1

--- exchange.ps1
+++ exchange.ps1
@@ -1,2 +1,5 @@
 #Set autoreply
 Set-MailboxAutoReplyConfiguration -Identity blha@blah.uk -AutoReplyState Enabled -InternalMessage "test" -ExternalMessage "test"
+
+#get mailbox permission user is assaigned to 
+Get-Mailbox | Get-MailboxPermission -User papajones | select-object -property *
